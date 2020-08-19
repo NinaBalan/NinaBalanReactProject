476ca7c804dadd7ac16738ddf9bfa105ac2c8ee6
NinaBalan <52449291+NinaBalan@users.noreply.github.com>
2020-08-19 17:56:56 +0300

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <Router>
+          <div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+            </Switch>
+          </div>
+        </Router>
+
+        <h1>NinaBalanReactProject</h1>
+        <PortfolioContainer />
+      </div>
+    );
+  }
+}
